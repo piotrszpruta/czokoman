8786aaa91aceabad14be5fcd9b7e4d6d18c64e74
piotrszpruta <38475451+piotrszpruta@users.noreply.github.com>
2020-11-06 14:12:01 +0100

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,8 +53,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "cf32356f65f594eaf68361dafd621019",
-"/": "cf32356f65f594eaf68361dafd621019",
+"index.html": "95e5e834577ef07c47410483d953cdde",
+"/": "95e5e834577ef07c47410483d953cdde",
 "main.dart.js": "b5b69ef5fadd713772e852434b1022d1",
 "manifest.json": "acc298f330807f9cf710aa57c0506b03",
 "version.json": "1b6adf7d28bded05383307a169afb8ac"
